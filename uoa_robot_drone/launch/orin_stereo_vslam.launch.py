4474ec9b30f355dc7a4d7af193a3cbfc33f48b88
Jian Chen <j.chen3.24@abdn.ac.uk>
2025-07-27 21:13:35 +0100

    modified the orin_stereo_vslam.launch.py according to http://wiki.ros.org/rtabmap_slam

--- uoa_robot_drone/launch/orin_stereo_vslam.launch.py
+++ uoa_robot_drone/launch/orin_stereo_vslam.launch.py
@@ -42,31 +42,48 @@ def generate_launch_description():
 
     # RTAB-Map parameters
     parameters={
-            'frame_id':'zed_camera_link',
-            'odom_frame_id': 'vo/odom',            # VO uses its own odometry frame to avoid conflicts
-            'base_frame_id': 'zed_camera_link',
-            'visual_odometry': True,  # Enable visual odometry
-            'icp_odometry': False,  # Disable ICP (Iterative Closest Point) odometry
-            'stereo': True,  # Enable stereo processing
-            'subscribe_rgbd': False,  # Subscribe to RGBD images
-            'subscribe_stereo': True,  # Disable stereo subscription since we are using RGBD images
-            'subscribe_odom_info': True,  # True: use internal odometry, False: use external odometry
-            'subscribe_scan': False,  # Disable scan subscription
-            'approx_sync': True,  # Enable approximate sync for stereo images
-            'Reg/strategy': '0',  # Registration strategy for loop closure, neighbor link refining, proximity detection. 0: Visual, 1: ICP, 2: Visual + ICP
-            'Rtabmap/TimeThr': '2000', # Maximum time (in milliseconds) that RTAB-Map is allowed to spend on processing each frame. Type value: 700
-            'Rtabmap/MemoryThr': '2000', # The threshold for the number of nodes retained in the working memory. When the number of nodes in the working memory exceeds this value, RTAB-Map will transfer older nodes to the long-term memory and may extract local representative nodes from it for subsequent loop closure detection, thereby optimizing memory usage.
-            'map_negative_poses_ignored': True,
-            'qos_image': 1, # 1：sensor_data（best_effort + volatile + keep_last 10）2：default（reliable + volatile + keep_last 10）
-            'Grid/RangeMin': '0.35',  # Minimum range for grid map
-            'Grid/Sensor': '1',  # 0=lidar, 1=camera/rgbd/pointcloud, 2=both.
-            'Reg/Force3DoF': 'true',  # Force 3 DoF registration
-            'wait_for_transform': 0.5, # Wait for transform to be available before processing images
+            # 'subscribe_depth': "true" (bool, default: "true") Subscribe to depth image.
+            'subscribe_scan': "false",  # (bool, default: "false") Subscribe to laser scan.
+            # 'subscribe_scan_cloud': "false" (bool, default: "false") Subscribe to laser scan point cloud.
+            'subscribe_stereo': True,  # (bool, default: "false") Subscribe to stereo images. Enable nodes (rtabmap_sync, rtabmap_odom, rtabmap_slam(rtabmap)) to subscribe to stereo images
+            'subscribe_rgbd': False,  # (bool, default: "false") Subscribe to rgbd_image topic, mainly involving nodes rtabmap_odom and rtabmap_slam(rtabmap).
+
+            'frame_id':'zed_camera_link', # (string, default: "base_link") The frame attached to the mobile base. The frame_id is the coordinate frame's name when the original visual data of the specific sensor is published, involving nodes rtabmap_sync, rtabmap_odom, rtabmap_slam(rtabmap).
+            # 'map_frame_id': "map" (string, default: "map") The frame attached to the map.
+            # 'odom_frame_id': 'vo/odom',  # (string, default: "") The frame attached to odometry. If empty, rtabmap will subscribe to odom topic to get odometry. If set, rtabmap will get odometry from the TF tree (odom_frame_id -> frame_id) and the convariance value is fixed by odom_tf_angular_variance and odom_tf_linear_variance.
+            # 'base_frame_id': 'zed_camera_link', # Not suitable for rtabmap. It is not a rtabmap parameter.  The base_frame_id is the coordinate frame's name for the robot's base, involving nodes rtabmap_odom and rtabmap_slam(rtabmap).
+            # 'visual_odometry': True,  # Not suitable for rtabmap. Enable visual odometry
+            # 'icp_odometry': False,  # Not suitable for rtabmap. ICP (Iterative Closest Point) odometry refers to the way of estimating the motion by aligning the closest points between two frames of the point clouds from different sensors iteratively, mainly involving node rtabmap_odom.
+            # 'subscribe_odom_info': True,  # Not suitable for rtabmap. True: use internal odometry, False: use external odometry
+            # 'subscribe_odom_info': False,  # Not suitable for rtabmap. Use external odometry
+            'wait_for_transform': "true", # (bool, default: "true") Wait (maximum wait_for_transform_duration sec) for transform when a tf transform is not still available. mainly involving nodes rtabmap_odom and rtabmap_slam(rtabmap). 
+            # wait_for_transform_duration (double, default: 0.1) Wait duration for wait_for_transform.
+            'gen_scan': "true", # (bool, default: "false") Generate laser scans from depth images (using the middle horizontal line of the depth image). Not generated if subscribe_scan or subscribe_scan_cloud are true.
+            'gen_scan_max_depth': "4.0", # (double, default: 4.0) Maximum depth of the laser scans generated.
+            'approx_sync': "false",  # (bool, default: "false") Use approximate time synchronization of input messages. If false, note that the odometry input must have also exactly the same timestamps than the input images. Enable approximate sync for stereo images, involving nodes (rtabmap_sync, rtabmap_odom, rtabmap_slam(rtabmap))
+            'odom_sensor_sync': "false",  # (bool, default: "false") Adjust image and scan poses relatively to odometry pose for each node added to graph. For example, if an image received at t=1s has been synchronized with a scan at t=1.1s (and our reference stamp is the scan) and the robot is moving forward at 1 m/s, then we will ask TF to know the motion between t=1s and t=1.1s (which would be 0.1 m) for the camera to adjust its local transform (-10 cm) relative to scan frame at scan stamp. This also applies to multi-camera synchronization.
+            'gen_depth_fill_holes_size': 5,  # (int, default: 0) Fill holes of empty pixels up to this size. Values are interpolated from neighbor depth values. 0 means disabled.
+            # 'gen_depth_fill_iterations': 0.1,  # (double, default: 0.1) Number of iterations to fill holes.
+            # 'gen_depth_fill_holes_error': 1,  # (int, default: 1) Maximum depth error (m) to interpolate.
+            # 'map_filter_radius': 0.0,  # (double, default: 0.0) Mapping Filter nodes before creating the maps. Only load data for one node in the filter radius (the latest data is used) up to filter angle (map_filter_angle). Set to 0.0 to disable node filtering. Used for all published maps.
+            # 'map_filter_angle': 30.0,  # (double, default: 30.0) Mapping Angle used when filtering nodes before creating the maps. See also map_filter_radius. Used for all published maps.
+            # 'latch': "true",  # (bool, default: "true") Mapping If true, the last message published on the map topics will be saved and sent to new subscribers when they connect.
+            'map_always_update': "true",  # (bool, default: "false") Mapping Always update the occupancy grid map even if the graph has not been updated (e.g., by default when the robot is not moving, the graph is not growing). For example, if the environment is dynamic, we may want to update the map even when to robot is not moving. Another approach would be to force rtabmap to always update the graph (by setting RGBD/LinearUpdate to 0), which can be not efficient over time as the map will grow even if the robot doesn't move.
+            'map_empty_ray_tracing': "true", # (bool, default: "true") Mapping Do ray tracing to fill unknown space for invalid 2D scan's rays (assuming invalid rays to be infinite). Used only when map_always_update is also true.
+            
+            'Reg/strategy': '0',  # Registration strategy for loop closure, neighbor link refining, proximity detection, involving node rtabmap_slam(rtabmap). 0: Visual, 1: ICP, 2: Visual + ICP
+            'Rtabmap/TimeThr': '2000', # Maximum time (in milliseconds) that RTAB-Map is allowed to spend on processing each frame, mainly involving node rtabmap_slam. Type value: 700
+            'Rtabmap/MemoryThr': '2000', # The threshold for the number of nodes retained in the working memory. When the number of nodes in the working memory exceeds this value, RTAB-Map will transfer older nodes to the long-term memory and may extract local representative nodes from it for subsequent loop closure detection, thereby optimizing memory usage, mainly involving node rtabmap_slam.
+            'map_negative_poses_ignored': True, # Ignore negative poses in the map, mainly involving node rtabmap_slam(rtabmap)
+            'qos_image': '1', # Quality of Service, 1：sensor_data（best_effort(no retransmission when missing packages) + volatile(the history is not retained when the node restarting) + keep_last 10(only cache the latest 10 messages)）2：default（reliable(the missing packages will be retransmitted) + volatile + keep_last 10）, mainly involving nodes rtabmap_sync, rtabmap_odom, rtabmap_slam(rtabmap).
+            'Grid/RangeMin': '0.0',  # Ignore points closer than this distance, mainly involving node rtabmap_slam(rtabmap).
+            'Grid/Sensor': '1',  # 0=lidar, 1=camera/rgbd/pointcloud, 2=both. Mainly involving node rtabmap_slam(rtabmap).
+            'Reg/Force3DoF': 'true',  # Force 3 DoF registration, mainly involving node rtabmap_slam(rtabmap).
             # RTAB-Map's internal parameters should be strings
-            'OdomF2M/MaxSize': '5000',
-            'Vis/MinInliers': '10',  # Lower the minimum inlier requirement
-            'GFTT/MinDistance': '10',
-            'GFTT/QualityLevel': '0.001',
+            'OdomF2M/MaxSize': '5000', # Maximum size of the OdomF2M buffer, mainly involving node rtabmap_odom.
+            'Vis/MinInliers': '10',  # Minimum number of inliers for visual matching between two images, the current image and the previous one, mainly involving nodes rtabmap_odom and rtabmap_slam(rtabmap).
+            'GFTT/MinDistance': '10', # Minimum distance between keypoints detected by GFTT (Good Features to Track), mainly involving nodes rtabmap_odom and rtabmap_slam(rtabmap).
+            'GFTT/QualityLevel': '0.001', # Quality level for GFTT keypoint detection, the higher the value, the fewer keypoints will be detected, mainly involving nodes rtabmap_odom and rtabmap_slam(rtabmap).
             #'Kp/DetectorStrategy': '6', # Uncommment to match ros1 noetic results, but opencv should be built with xfeatures2d
             #'Vis/FeatureType': '6'      # Uncommment to match ros1 noetic results, but opencv should be built with xfeatures2d
     }
@@ -76,9 +93,9 @@ def generate_launch_description():
         # ('odom_rgbd_image', '/stereo_camera/rgbd_image'),
         # ('global_pose', '/zed/zed_node/pose_with_covariance'),
         # ('imu', '/zed/zed_node/imu/data'),
-        # ('odom', '/zed/zed_node/odom'), # External odometry topic
-        ('rgbd_image', '/stereo_camera/rgbd_image'), # From internal stereo sync node
-        ('odom', '/vo/odom'), # Internal odometry topic from stereo_odometry node
+        # ('odom', '/vo/odom'), # Internal odometry topic from stereo_odometry node
+        ('odom', '/zed/zed_node/odom'), # External odometry topic
+        # ('rgbd_image', '/stereo_camera/rgbd_image'), # From internal stereo sync node
         ('left/image_rect', '/uoa/orin/left/image_rect_bgr'),
         ('right/image_rect', '/uoa/orin/right/image_rect_bgr'),
         ('left/camera_info', '/zed/zed_node/left/camera_info'), 
@@ -254,21 +271,21 @@ def generate_launch_description():
             ]
         ),
 
-        # 11) Synchronize stereo streams
-        Node(
-            package='rtabmap_sync', executable='stereo_sync', output='screen',
-            namespace='stereo_camera',
-            name='stereo_sync',
-            parameters=[parameters],
-            remappings=remappings
-        ),
+        # # 11) Synchronize stereo streams
+        # Node(
+        #     package='rtabmap_sync', executable='stereo_sync', output='screen',
+        #     namespace='stereo_camera',
+        #     name='stereo_sync',
+        #     parameters=[parameters],
+        #     remappings=remappings
+        # ),
 
         # 12) Visual Odometry (VO) node
-        Node(
-            package='rtabmap_odom', executable='stereo_odometry', output='screen',
-            parameters=[parameters],
-            remappings=remappings
-        ),
+        # Node(
+        #     package='rtabmap_odom', executable='stereo_odometry', output='screen',
+        #     parameters=[parameters],
+        #     remappings=remappings
+        # ),
 
         # 13) SLAM node
         Node(
